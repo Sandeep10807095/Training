6557c08dd8c75e3686b2b3b2bc981a5ba429f936
SandeepSrinivasDwaram <sandeepsrinivasdwaram@github.com>
2019-06-24 12:06:07 +0200

    Created AlbumList

--- thirdapp/src/components/Header.js
+++ thirdapp/src/components/Header.js
@@ -1,11 +1,12 @@
-import react from 'react';
-import { link } from 'react-router-dom';
+import React from'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-    return (
-        <Header>
-            <link to="/">Music DB</link>
-        </Header>
+    return(
+        <header>
+            <Link to="/">Music DB</Link>
+        </header>
     )
 }
+
 export default Header;
