2ee18245458a677194fbb6d8138c2484e6d8e5c2
SandeepSrinivasDwaram <sandeepsrinivasdwaram@github.com>
2019-06-25 05:18:15 +0200

    Redux Introduction

---
+++ redux1/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+import { Provider} from 'react-redux';
+import { createStore, applyMiddleware} from 'react';
+import reducers from './reducer'
+
+const createStoreWithMiddleware =  applyMiddleware()(createStore);
+
+
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App/>
+    </Provider>, document.getElementById('root'));
