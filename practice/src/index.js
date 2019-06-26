3b1e49b269f33cf94e98ee68b83c4e74535e6a59
SandeepSrinivasDwaram <sandeepsrinivasdwaram@github.com>
2019-06-26 11:25:35 +0200

    added Header and Footer

--- practice/src/index.js
+++ practice/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import xreactDOM from 'react-dom';
-import routing from './components/routing';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Routing/>),document.getElementById('root');
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
